8871a82be264057d9134a3a3e3a18c0e2dfc3529
Hatschkim <33868216+MariusStuber@users.noreply.github.com>
2018-05-03 10:58:26 +0200

    Add files via upload

---
+++ Code/Components.java
@@ -0,0 +1,38 @@
+public class Components {
+	private static String all_names[] = {
+			"assassin",
+			"black",
+			"blade",
+			"dagger",
+			"dark",
+			"death",
+			"destroyer",
+			"ghost",
+			"hunter",
+			"killer",
+			"lord",
+			"night",
+			"reaper",
+			"shadow",
+			"slayer",
+			"soul"
+	};
+
+	private static String first_names[] = {
+			"grim",
+			"over"
+	};
+
+	private static String second_names[] = {
+	};
+
+	public static String[] getAll() {
+		return all_names;
+	}
+	public static String[] getFirst() {
+		return first_names;
+	}
+	public static String[] getSecond() {
+		return second_names;
+	}
+}
